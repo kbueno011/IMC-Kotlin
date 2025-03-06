cd7931a4a5696c613cc1a21591737057f0b1bddb
KAIKE BUENO <24122303@dsn127m1249387.cfp127aluno.local>
2025-03-06 17:14:08 -0300

    tela HomeScreen concluida

--- app/src/main/java/br/senai/sp/jandira/bmi/screens/HomeScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/HomeScreen.kt
@@ -9,31 +9,42 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.bmi.R
 
+
+
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+
+    var nomeState = remember {
+        mutableStateOf(value = "")
+    }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -95,13 +106,37 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                             color = Color.Black
                         )
                         OutlinedTextField(
-                            value = "",
-                            onValueChange = {},
+                            value = nomeState.value,
+                            onValueChange = {
+
+                                nomeState.value = it
+
+                            },
                             label = {
                                 Text(text = "Digite o seu nome")
                             },
                             modifier = Modifier
-                                .fillMaxWidth()
+                                .fillMaxWidth(),
+                            leadingIcon = {
+                               Icon(
+                                   imageVector = Icons.Default.Email,
+                                   contentDescription = "",
+                                   tint = Color(0xFF1644BB)
+
+                               )
+                            },
+                            trailingIcon = {
+                                Icon(
+                                    imageVector =  Icons.Default.LocationOn,
+                                    contentDescription = "",
+                                    tint = Color(0xFF1644BB)
+                                )
+                            },
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Text,
+                                capitalization = KeyboardCapitalization.Sentences
+                            )
+
                         )
                     }
 
@@ -109,12 +144,10 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                         onClick = {},
                         shape = RoundedCornerShape(10.dp)
 
-                        ) {
-                        Text(text = stringResource(R.string.next))
-                        Icon(
-                            imageVector = Icons.Filled.ArrowForward,
-                            contentDescription = ""
                         )
+                    {
+                        Text(text = stringResource(R.string.next))
+
                     }
                 }
 
---
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/UserScreen.kt
@@ -0,0 +1,18 @@
+package br.senai.sp.jandira.bmi.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun UserScreen(modifier: Modifier = Modifier) {
+
+
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun HomeScreenPreview() {
+    UserScreen()
+}
